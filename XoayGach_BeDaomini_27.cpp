6c80b5ea8226941aaa722990c620dbcb2397dcac
Your Name <you@example.com>
2026-07-02 01:31:55 +0700

    add

---
+++ XoayGach_BeDaomini_27.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define int ll
+using namespace std;
+
+const int INF = (int)1e9 + 100;
+const int N = (int)1e6 + 5;
+double C, p, n1, n2, t1, t2;
+
+signed main(){
+    int n;
+    cin >> n;
+    if(n % 2 == 0) {
+        cout << "YES";
+    }
+    else cout << "NO";
+    
+}
